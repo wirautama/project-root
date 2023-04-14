66c5cbaa5717d3b51613a9ab21b69cf9f49d570b
unknown <wirautama>
2023-04-14 07:11:29 +0700

    fix bug change password

--- app/Controllers/ChangePassword.php
+++ app/Controllers/ChangePassword.php
@@ -6,6 +6,8 @@ use App\Controllers\BaseController;
 use App\Models\UserModel;
 use Myth\Auth\Password;
 
+use function PHPUnit\Framework\matches;
+
 class ChangePassword extends BaseController
 {
     protected $userModel;
@@ -28,9 +30,16 @@ class ChangePassword extends BaseController
 
     public function updatePassword($id = null)
     {
+        $validationOldPassword = password_verify($this->request->getVar('password_lama'), $this->request->getVar('passwordLama'));
+        if ($validationOldPassword == true) {
+            $rule_password_lama = 'required|matches[users.password_hash]';
+        } else {
+            $rule_password_lama = 'required';
+        }
+
 
         if (!$this->validate([
-            'password_lama' => 'required',
+            'password_lama' => $rule_password_lama,
             'password_baru' => 'required|strong_password',
             'konfirm_password_baru' => 'required|matches[password_baru]'
         ])) {
